97443e872fff62eafb8178ca732931642cf2f639
Raul-Araujo <raul31_lpcn@hotmail.com>
2020-10-04 01:15:03 -0300

    finishing todays work

--- assets/script.js
+++ assets/script.js
@@ -57,11 +57,34 @@ class Calculator {
         this.previousOperand = '';
     }
 
-    updateDisplay(){
-        this.currentOperandTextElement.innerText = this.currentOperand;
-        if(this.operation != null){
-            this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`;
+    getDisplayNumber(number){
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split('.')[0]);
+        const decimalDigits = stringNumber.split('.')[1];
+        let integerDislay;
 
+        if(isNaN(integerDigits)) {
+            integerDisplay = '';
+        } else {
+            integerDisplay = integerDigits.toLocaleString('pt-BR', {
+                maximumFractionDigits: 0
+            })
+        }
+
+        if(decimalDigits != null){
+            return `${integerDisplay}.${decimalDigits}`;
+        } else {
+            return integerDisplay;
+        }
+    }
+
+    updateDisplay(){
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand);
+        if(this.operation != null){
+            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
+
+        } else {
+            this.previousOperandTextElement.innerText = '';
         }
     }
 }
